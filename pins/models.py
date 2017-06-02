1ae227f7f8ed515d94ade92d7550b778ccd2b501
William Ray <wor15@ic.ac.uk>
2017-06-02 14:06:31 +0100

    Pins expire, api tests removed

--- pins/models.py
+++ pins/models.py
@@ -1,3 +1,6 @@
+import datetime
+
+from django.utils import timezone
 from django.db import models
 
 class PinCode(models.Model):
@@ -6,6 +9,9 @@ class PinCode(models.Model):
     station = models.PositiveIntegerField()
 
     creation = models.DateTimeField(auto_now_add=True)
+
+    def is_valid(self):
+        return (timezone.now() - self.creation).total_seconds() < 3600
 
     def __str__(self):
         return '{:06d}'.format(self.pin_code)
--- pins/tests/test_model.py
+++ pins/tests/test_model.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.test import TestCase
+from django.utils import timezone
 
 from ..models import PinCode
 
@@ -8,3 +11,14 @@ class PinCodeModelTests(TestCase):
         pin_code = PinCode(pin_code=23, voter=1, station=1)
 
         self.assertEqual(str(pin_code), '000023')
+
+    def test_pin_valid_when_made(self):
+        PinCode(pin_code=23, voter=1, station=1).save()
+
+        self.assertTrue(PinCode.objects.get(pin_code=23).is_valid())
+
+    def test_pin_invalid_after_an_hour(self):
+        hour_ago = timezone.now() - datetime.timedelta(minutes=60)
+        pin_code = PinCode(pin_code=23, voter=1, station=1, creation=hour_ago)
+
+        self.assertFalse(pin_code.is_valid())
