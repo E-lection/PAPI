aae49538bbe9535a08cb025d901224aaa25074d3
William Ray <wor15@ic.ac.uk>
2017-06-13 18:20:43 +0100

    Voter ineligiblity API call made

--- pins/api_utils.py
+++ pins/api_utils.py
@@ -4,7 +4,7 @@ import json
 
 VOTER_API_URL = 'http://voting.eelection.co.uk/check_votable/'
 PIN_ACTIVATION_URL = 'http://voting.eelection.co.uk/set_voter_has_active_pin/'
-
+VOTER_INELIGIBLE_URL = 'http://voting.eelection.co.uk/make_voter_ineligible/'
 
 def check_votablity(votability_data):
     # Returns TRUE if someone can vote
@@ -23,8 +23,9 @@ def get_and_check_votability(voter_id):
 
 
 def make_voter_ineligible(voter_id):
-    # TODO
-    return True
+    url = VOTER_INELIGIBLE_URL + urllib.quote(str(voter_id)) + '/'
+    response = urllib2.urlopen(url)
+    return json.loads(response.read())['success']
 
 
 def activate_pin(voter_id):
