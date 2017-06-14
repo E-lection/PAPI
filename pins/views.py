6fe0bbc6b2f8edbdb6e6a4d4e9e58bc0c44d6fdc
William Ray <wor15@ic.ac.uk>
2017-06-14 13:21:07 +0100

    Updated ineligibility to block

--- pins/views.py
+++ pins/views.py
@@ -33,7 +33,7 @@ def verify_pin_code_and_check_eligibility(request, station_id, pin_code):
 
 def verify_pin_code_and_make_ineligible(request, station_id, pin_code):
     try:
-        pin_object = PinCode.objects.get(station=station_id, pin_code=pin_code)
+        pin_object = PinCode.objects.select_for_update().get(station=station_id, pin_code=pin_code)
         # Pin exists #
         if get_and_check_votability(pin_object.voter) and make_voter_ineligible(pin_object.voter):
             PinCode.objects.get(station=station_id, pin_code=pin_code).delete()
